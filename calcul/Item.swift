102c1125016ae7ff3335ca878ea61f9855fa3da4
jesus <jesusteactivat23@gmail.com>
2024-05-20 13:49:43 +0200

    Initial Commit

---
+++ calcul/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  calcul
+//
+//  Created by Jesús Navarro Antolin on 20/5/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
